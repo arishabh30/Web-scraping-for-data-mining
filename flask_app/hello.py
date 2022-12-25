df173dff2eddc9da77b4b7f89c5a3502b7b35123
Swayam Gupta <77153230+swayamg20@users.noreply.github.com>
2022-12-25 23:50:22 +0530

    relationship udated

--- flask_app/hello.py
+++ flask_app/hello.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, url_for, request
 from source import *
 import csv
@@ -5,57 +7,232 @@ import numpy as np
 import pandas as pd
 app = Flask(__name__)
 
-@app.route("/", methods =["GET", "POST"])
-def index():
-    if request.method == "POST":
-        url = request.form.get("enteredurl")
-        print(url)
-        html = gettingUrl(url)
-        print(html)
 
-        string = url.split(".")
+# DOIList_main =[]
 
-        if "springer" in string:
-            auth, titles, journalList, yearlist, DOIs, scholarLinks = Springer(html)
-        
-        elif "nature" in string:
-            auth, titles, journalList, yearlist, DOIs, scholarLinks = Nature(html) 
+@app.route('/<string:doi_link>', methods=['GET', 'POST'])
+def home(doi_link):
 
-        elif "science" in string and "sciencedirect" not in string:
-            auth, titles, journalList, yearlist, DOIs, scholarLinks = Science(html)
+    # print(int(doi_link))
 
-        elif "mdpi" in string:
-            auth, titles, journalList, yearlist, DOIs, scholarLinks = MDPI(html)
+    # print(index.DOIList_main)
+    q =request.args.get("q")
+    print(q)
 
-        elif "ieee" in string:
-            url = url + "/references#references"
-            auth, titles, journalList, yearlist, DOIs, scholarLinks = IEEE(html)
-        
-        elif 'cambridge' in string:
-            auth, titles, journalList, yearlist, DOIs, scholarLinks = Cambridge(html)
-        
-        elif 'sciencedirect' in string:
-            auth, titles, journalList, yearlist, DOIs, scholarLinks = ScienceDirect(html)
-        elif 'acs' in string:
-            auth, titles, journalList, yearlist, DOIs, scholarLinks = ACS(html)
+    url = q
+    link = doi_link
+    seperate1 = link.split('https:$$doi.org$')
+    print(seperate1)
+    print(url)
+    html = gettingUrl(url)
 
+    # print(html)
 
-        
+    string = url.split('.')
+    if 'springer' in string:
+        (
+            auth,
+            titles,
+            journalList,
+            yearlist,
+            DOIs,
+            scholarLinks,
+            ) = Springer(html)
+    elif 'nature' in string:
 
-        
+        (
+            auth,
+            titles,
+            journalList,
+            yearlist,
+            DOIs,
+            scholarLinks,
+            ) = Nature(html)
+    elif 'science' in string and 'sciencedirect' not in string:
+
+        (
+            auth,
+            titles,
+            journalList,
+            yearlist,
+            DOIs,
+            scholarLinks,
+            ) = Science(html)
+    elif 'mdpi' in string:
+
+        (
+            auth,
+            titles,
+            journalList,
+            yearlist,
+            DOIs,
+            scholarLinks,
+            ) = MDPI(html)
+    elif 'ieee' in string:
+
+        url = url + '/references#references'
+        (
+            auth,
+            titles,
+            journalList,
+            yearlist,
+            DOIs,
+            scholarLinks,
+            ) = IEEE(html)
+    elif 'cambridge' in string:
+
+        (
+            auth,
+            titles,
+            journalList,
+            yearlist,
+            DOIs,
+            scholarLinks,
+            ) = Cambridge(html)
+    elif 'sciencedirect' in string:
+
+        (
+            auth,
+            titles,
+            journalList,
+            yearlist,
+            DOIs,
+            scholarLinks,
+            ) = ScienceDirect(html)
+    elif 'acs' in string:
+        (
+            auth,
+            titles,
+            journalList,
+            yearlist,
+            DOIs,
+            scholarLinks,
+            ) = ACS(html)
 
         # dict = {'Author':auth,'Title':titles,'Journal':journalList,'Year of publication':yearlist,'DOI number':DOIs,'Scholar Links':scholarLinks}
+        # df = pd.DataFrame(dict)
+        # # saving the dataframe
+        # df.to_csv('content.csv')
 
-        # df = pd.DataFrame(dict) 
-        # # saving the dataframe 
-        # df.to_csv('content.csv') 
-            
-      
-        
-        
+    return render_template(
+        'link.html',
+        name=url,
+        author=auth,
+        titleAll=titles,
+        DOIList=DOIs,
+        YearList=yearlist,
+        journals=journalList,
+        array=scholarLinks,
+        )
 
-        return render_template('index.html', name=url, author=auth, titleAll = titles,DOIList =DOIs, YearList = yearlist, journals = journalList, array = scholarLinks)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form.get('enteredurl')
+        print(url)
+        html = gettingUrl(url)
+
+        # print(html)
+
+        string = url.split('.')
+
+        if 'springer' in string:
+            (
+                auth,
+                titles,
+                journalList,
+                yearlist,
+                index.DOIList_main,
+                scholarLinks,
+                ) = Springer(html)
+        elif 'nature' in string:
+
+            (
+                auth,
+                titles,
+                journalList,
+                yearlist,
+                index.DOIList_main,
+                scholarLinks,
+                ) = Nature(html)
+        elif 'science' in string and 'sciencedirect' not in string:
+
+            (
+                auth,
+                titles,
+                journalList,
+                yearlist,
+                index.DOIList_main,
+                scholarLinks,
+                ) = Science(html)
+        elif 'mdpi' in string:
+
+            (
+                auth,
+                titles,
+                journalList,
+                yearlist,
+                index.DOIList_main,
+                scholarLinks,
+                ) = MDPI(html)
+        elif 'ieee' in string:
+
+            url = url + '/references#references'
+            (
+                auth,
+                titles,
+                journalList,
+                yearlist,
+                index.DOIList_main,
+                scholarLinks,
+                ) = IEEE(html)
+        elif 'cambridge' in string:
+
+            (
+                auth,
+                titles,
+                journalList,
+                yearlist,
+                index.DOIList_main,
+                scholarLinks,
+                ) = Cambridge(html)
+        elif 'sciencedirect' in string:
+
+            (
+                auth,
+                titles,
+                journalList,
+                yearlist,
+                index.DOIList_main,
+                scholarLinks,
+                ) = ScienceDirect(html)
+        elif 'acs' in string:
+            (
+                auth,
+                titles,
+                journalList,
+                yearlist,
+                index.DOIList_main,
+                scholarLinks,
+                ) = ACS(html)
+
+        # dict = {'Author':auth,'Title':titles,'Journal':journalList,'Year of publication':yearlist,'DOI number':DOIs,'Scholar Links':scholarLinks}
+        # df = pd.DataFrame(dict)
+        # # saving the dataframe
+        # df.to_csv('content.csv')
+
+        return render_template(
+            'index.html',
+            name=url,
+            author=auth,
+            titleAll=titles,
+            DOIList=index.DOIList_main,
+            YearList=yearlist,
+            journals=journalList,
+            array=scholarLinks,
+            )
     return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
